3ccae95eddda49d3182328f5308cc0f464770e3d
Jonathan Alber <>
2026-05-23 15:08:47 +0200

    [UPD] Updater window now also works with selectable packages.

--- Scripts/Editor/BasePackageUpdaterWindow.cs
+++ Scripts/Editor/BasePackageUpdaterWindow.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -10,9 +11,11 @@ namespace Base.PackageInstaller.Editor
     /// </summary>
     public class BasePackageUpdaterWindow : EditorWindow
     {
-        private PackageUpdater _updater;
+        private readonly bool[] _selected = Enumerable.Repeat(true, BasePackageRegistry.Packages.Length).ToArray();
 
         private string _status;
+        private Vector2 _scroll;
+        private PackageUpdater _updater;
 
         [MenuItem("Tools/Base Package Installer/Updater")]
         public static void ShowWindow() => GetWindow<BasePackageUpdaterWindow>("Base Package Updater");
@@ -39,6 +42,8 @@ namespace Base.PackageInstaller.Editor
         {
             DrawNavigation();
 
+            DrawPackagesSection();
+
             EditorGUILayout.Space(8);
 
             EditorGUILayout.LabelField("Base Package Updater", EditorStyles.boldLabel);
@@ -50,13 +55,6 @@ namespace Base.PackageInstaller.Editor
                 MessageType.Info);
 
             EditorGUILayout.Space(8);
-
-            EditorGUI.BeginDisabledGroup(_updater.IsRunning);
-
-            if (GUILayout.Button("Update All Packages", GUILayout.Height(30)))
-                StartUpdate();
-
-            EditorGUI.EndDisabledGroup();
 
             if (string.IsNullOrEmpty(_status))
                 return;
@@ -82,9 +80,45 @@ namespace Base.PackageInstaller.Editor
             EditorGUILayout.Space(4);
         }
 
-        private void StartUpdate()
+        private void DrawPackagesSection()
         {
-            _updater.Run(BasePackageRegistry.Packages.Select(x => x.Url));
+            EditorGUILayout.Space(8);
+            EditorGUILayout.LabelField("Base Packages", EditorStyles.boldLabel);
+            EditorGUILayout.Space(4);
+
+            _scroll = EditorGUILayout.BeginScrollView(_scroll);
+
+            for (int i = 0; i < BasePackageRegistry.Packages.Length; i++)
+                _selected[i] = EditorGUILayout.ToggleLeft(BasePackageRegistry.Packages[i].Name, _selected[i]);
+
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.Space(8);
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Select All"))
+                SetAllSelected(true);
+
+            if (GUILayout.Button("Deselect All"))
+                SetAllSelected(false);
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(4);
+
+            EditorGUI.BeginDisabledGroup(_updater.IsRunning);
+
+            if (GUILayout.Button("Update Selected", GUILayout.Height(30)))
+                StartUpdate();
+
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private void SetAllSelected(bool value)
+        {
+            for (int i = 0; i < _selected.Length; i++)
+                _selected[i] = value;
         }
 
         private void HandlePackageStarted(string url)
@@ -111,6 +145,17 @@ namespace Base.PackageInstaller.Editor
         {
             _status = "All packages updated successfully.";
             Repaint();
+        }
+
+        private void StartUpdate()
+        {
+            List<string> urls = new();
+
+            for (int i = 0; i < BasePackageRegistry.Packages.Length; i++)
+                if (_selected[i])
+                    urls.Add(BasePackageRegistry.Packages[i].Url);
+
+            _updater.Run(urls);
         }
     }
 }
