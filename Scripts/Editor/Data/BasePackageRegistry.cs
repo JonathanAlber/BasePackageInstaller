3a2a81e210ffda6bcd76ca4bc580edd777fc9ed4
Jonathan Alber <>
2026-06-07 19:38:26 +0200

    [ADD] Localization package.

--- Scripts/Editor/Data/BasePackageRegistry.cs
+++ Scripts/Editor/Data/BasePackageRegistry.cs
@@ -10,14 +10,51 @@ namespace Base.PackageInstaller.Editor.Data
     {
         public static readonly PackageEntry[] Packages =
         {
-            new("Tools", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Tools"),
-            new("Attributes", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Attributes"),
-            new("Systems", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Systems"),
-            new("UI", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/UI"),
-            new("Utility", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Utility"),
-            new("ScreenShake", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/ScreenShake"),
-            new("Save System", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/SaveSystem"),
-            new("Settings System", "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Settings"),
+            new
+            (
+                "Tools",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Tools"
+            ),
+            new
+            (
+                "Attributes",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Attributes"
+            ),
+            new
+            (
+                "Systems",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Systems"
+            ),
+            new
+            (
+                "UI",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/UI"
+            ),
+            new
+            (
+                "Utility",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Utility"
+            ),
+            new
+            (
+                "ScreenShake",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/ScreenShake"
+            ),
+            new
+            (
+                "Save System",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/SaveSystem"
+            ),
+            new
+            (
+                "Settings System",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Settings"
+            ),
+            new
+            (
+                "Localization",
+                "https://github.com/JonathanAlber/BaseProjectPackages.git?path=BaseProject/Packages/Localization"
+            ),
         };
 
         public static readonly PackageEntry[] SortedPackages = Packages.OrderBy(p => p.Name).ToArray();
